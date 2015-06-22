486f41ac9d335bd6de859389636a3b1523e34442
zhouyilong <1023471981@qq.com>
2015-06-22 23:56:58 +0800

    新增提示功能

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoCloseComputer.Storage;
+using System;
+using System.Data;
+using System.Data.OleDb;
 using System.Diagnostics;
 using System.Windows;
 
@@ -9,9 +12,32 @@ namespace AutoCloseComputer
     /// </summary>
     public partial class MainWindow : Window
     {
+        DateTime closeDateTime;//数据库中的关机时间
+
         public MainWindow()
         {
             InitializeComponent();
+
+            using( OleDbConnection conn = AccessHelper.getConn()) //getConn():得到连接对象
+            {
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
+                string sqlstr = "Select * from CloseDate where ID=1";
+                conn.Open();
+                DataSet mydataset = new DataSet();
+                adapter.SelectCommand = new OleDbCommand(sqlstr, conn);
+                adapter.Fill(mydataset, "CloseDate");
+                string strCloseDateTime=Convert.ToString(mydataset.Tables["CloseDate"].Rows[0]["CDate"]);
+                if (DateTime.TryParse(strCloseDateTime, out closeDateTime))
+                {
+                    if(closeDateTime>DateTime.Now)
+                    {
+                        StartShutDown.IsEnabled = false;
+                        StopShutDown.IsEnabled = true;
+
+                        showTime.Text = "小龙温馨提示：\n当前计算机将在" + strCloseDateTime+"关闭\n请如果需要取消定时关机，请点击取消定时关机！";
+                    }
+                }
+            }
         }
 
         //开始定时关机
@@ -47,7 +73,26 @@ namespace AutoCloseComputer
                 //p.WaitForExit();
                 //p.Close();
                 //MessageBox.Show(strMessage);
-           
+
+                //将关机时间更新到数据库中
+                DateTime dtCloseTime = DateTime.Now.AddSeconds(minutes * 60 + hours * 60 * 60);
+                string strCloseTime = dtCloseTime.ToString();
+                using (OleDbConnection conn = AccessHelper.getConn()) //getConn():得到连接对象
+                {
+                    conn.Open();
+                    string strSQL = "update CloseDate set CDate='" + strCloseTime + "' where ID=1";
+                    //定义command对象，并执行相应的SQL语句
+                    OleDbCommand myCommand = new OleDbCommand(strSQL, conn);
+                    myCommand.ExecuteNonQuery(); 
+                }
+
+                if (dtCloseTime > DateTime.Now)
+                {
+                    StartShutDown.IsEnabled = false;
+                    StopShutDown.IsEnabled = true;
+
+                    showTime.Text = "小龙温馨提示：\n当前计算机将在" + strCloseTime + "关闭\n请如果需要取消定时关机，请点击取消定时关机！";
+                }
             }
             else
             {
@@ -68,6 +113,20 @@ namespace AutoCloseComputer
             p.StartInfo.CreateNoWindow = true;
             p.Start();
             p.StandardInput.WriteLine("shutdown -a");   //填CMD命令
+
+            using (OleDbConnection conn = AccessHelper.getConn()) //getConn():得到连接对象
+            {
+                conn.Open();
+                string strSQL = "update CloseDate set CDate='1900/1/1 00:00:00' where ID=1";
+                //定义command对象，并执行相应的SQL语句
+                OleDbCommand myCommand = new OleDbCommand(strSQL, conn);
+                myCommand.ExecuteNonQuery(); 
+            }
+
+            StartShutDown.IsEnabled = true;
+            StopShutDown.IsEnabled = false;
+
+            showTime.Text = "已经取消了当前定时计划！";
         }
 
     }
---
+++ Storage/AccessHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCloseComputer.Storage
+{
+    public class AccessHelper
+    {
+        public static OleDbConnection getConn()
+        {
+            //string connstr = "Provider= Microsoft.ACE.OLEDB.12.0;Data Source=./AutoCloseComputerDB.accdb";
+            string connstr = "Provider= Microsoft.Jet.OLEDB.4.0;Data Source=./AutoCloseComputerDB.mdb";
+            OleDbConnection tempconn = new OleDbConnection(connstr);
+            return (tempconn);
+        }
+    }
+}
